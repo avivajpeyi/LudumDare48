73dcbe2cdcfc1ab70b4d555f1cf1193e17383d4a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-04-24 19:42:31 +1000

    add arena components

---
+++ Assets/Scripts/ArenaManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArenaManager : MonoBehaviour
+{
+    public ArenaComponentsManager entityManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AutomatedRestart();
+    }
+
+
+    void ResetArena()
+    {
+        Debug.Log("Arena Reset");
+        entityManager.ResetArenaComponents();
+    }
+
+
+    void AutomatedRestart()
+    {
+        InvokeRepeating("ResetArena", time: 0.5f, repeatRate: 3);
+    }
+}
