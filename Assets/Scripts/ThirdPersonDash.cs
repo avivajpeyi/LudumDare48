8d4a94b0ea519b1cc49cc427dabca786cbedf6fa
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-04-24 18:06:31 +1000

    add basic controller with dash

---
+++ Assets/Scripts/ThirdPersonDash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonDash : MonoBehaviour
+{
+    private ThirdPersonMovement moveScript;
+    public float dashSpeed = 20.0f;
+    public float dashTime = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveScript = GetComponent<ThirdPersonMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("DASH");
+            StartCoroutine(Dash());
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+        while (Time.time < startTime + dashTime)
+        {
+            moveScript.controller.Move(
+                moveScript.moveDir * dashSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+    }
+}
