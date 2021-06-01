08e68f3e09dd138cf4d349b7522ee180e78c9eb1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-01 23:06:41 +1000

    reimport click controller

---
+++ Assets/Scripts/CharacterControllers/ClickController/RotateObjectBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Objective arrow
+/// </summary>
+public class RotateObjectBehaviour : MonoBehaviour
+{
+    [SerializeField] protected float m_RotationSpeed;
+
+
+    private float m_StartPosAxis;
+
+
+    //Bounce the indicator up and down, and rotate it.
+    void Update()
+    {
+        //transform.Rotate(Vector3.right * (m_RotationSpeed * Time.deltaTime));
+        transform.Rotate(Vector3.up * (m_RotationSpeed * Time.deltaTime));
+    }
+}
